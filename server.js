5cdb978d787ec46a54ba5d9996fc772e1ddd805e
SazumiVicky <vicihidayat722@gmail.com>
2024-03-27 06:55:14 -0700

    next

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+/*
+* dev: Sazumi Viki
+* ig: @moe.sazumiviki
+* gh: github.com/sazumivicky
+* site: sazumi.moe
+*/
+
 const { remote } = require('webdriverio');
 const express = require('express');
 const bodyParser = require('body-parser');
